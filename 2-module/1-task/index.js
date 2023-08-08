be4b7c355831b7e7433421cd8b08da1761b8214d
Aleksandr Bezverchuk <albezver@mail.com>
2023-08-08 18:59:14 -0500

    Created a function to summarize salaries

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,23 @@
-function sumSalary(salaries) {
-  // ваш код...
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false,
+  bonus: NaN,
+  penalty: Infinity
 }
+
+function sumSalary(salaries) {
+  let salariesSum = 0;
+  
+  for (key in salaries) {
+    if ( typeof salaries[key] === "number" && isFinite(salaries[key]) ){
+      salariesSum += salaries[key];
+    }
+  }
+  
+  return salariesSum;
+}
+sumSalary(salaries);
