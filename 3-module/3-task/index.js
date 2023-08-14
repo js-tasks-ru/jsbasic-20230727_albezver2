8ef694c01c218c4c6dc1532c5a6519e81404d938
Aleksandr Bezverchuk <albezver@mail.com>
2023-08-13 20:52:55 -0500

    Created a function to change a text to UpperCase for certain letters

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,14 @@
 function camelize(str) {
-  // ваш код...
+  let splitted = str.split('');
+  
+  for (let i = 0; i < splitted.length; i++) {
+    if (splitted[i] === "-") {
+      splitted.splice(i, 1);
+      if (i < splitted.length) {
+        splitted[i] = splitted[i].toUpperCase();
+      }
+    }
+  }
+  return splitted.join("");
 }
+
